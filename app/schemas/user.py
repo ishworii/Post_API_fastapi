68b1786122aeb2b9ad4583ceed19f28243c8b554
ishworii <ishworkhanal21@gmail.com>
2024-09-23 09:08:17 +0545

    Removed Post list from user schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, EmailStr
-from app.schemas.post import PostRead
 
 
 class UserBase(BaseModel):
@@ -14,7 +13,6 @@ class UserCreate(UserBase):
 
 class UserRead(UserBase):
     id: int
-    posts: list[PostRead] = []
 
     class Config:
         orm_mode = True
