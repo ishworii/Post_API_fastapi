cda915e30a9a08e803fad47b75c8622e8e103d94
ishworii <ishworkhanal21@gmail.com>
2024-10-21 11:17:10 +0545

    Added barebone frontend

---
+++ svelte-frontend/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  let posts = [];
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/posts");
+      posts = await res.json();
+      console.log(posts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  // Fetch posts when the component is mounted
+  fetchPosts();
+</script>
+
+<main>
+  <h1>Posts</h1>
+  {#if posts.length > 0}
+    <ul>
+      {#each posts as post}
+        <li>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No posts available</p>
+  {/if}
+</main>
